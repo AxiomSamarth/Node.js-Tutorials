41136cd03bd6d448d066b1baf4e84e35ac16f938
Samarth Deyagond <deyagondsamarth@gmail.com>
2019-07-26 10:57:35 +0530

    Added test framework jasmine and server.spec.js test case descriptor file.

--- Chapter_4/server.js
+++ Chapter_4/server.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+mongoose.Promise = Promise
 var dbUrl = 'mongodb://localhost:27017/learning-node'
 var Message = mongoose.model('messages', {
     name: String,
@@ -18,19 +19,39 @@ var Message = mongoose.model('messages', {
 var messages = []
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
-app.post('/messages', (req, res) => {
-    var message = new Message(req.body)
-    message.save((err) => {
-        if(err){
-            sendStatus(500)
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({name:user}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.post('/messages', async (req, res) => {
+
+    try {
+        var message = new Message(req.body)
+        var savedMessage = await message.save()        
+        var censored = await Message.findOne({message: 'badword'})
+        
+        if(censored){
+            await Message.deleteOne({_id: censored.id})
+        }else{   
+            io.emit('message', req.body)
         }
-        messages.push(req.body)    
-        io.emit('message', req.body)
-        res.status(200).send([req.body])
-    })    
+        messages.push(req.body) 
+        res.status(200).send([req.body])         
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally{
+        console.log('Message post called')
+    }
+           
 })
 
 io.on('connection', (socket) => {
---
+++ Chapter_4/spec/server.spec.js
@@ -0,0 +1,33 @@
+var request = require('request')
+
+describe('Calc', () => {
+    it('should multiple 2 and 2', () => {
+        expect(2*2).toBe(4)
+    })
+})
+
+describe('get messages', () => {
+    it('should return 200 OK', (done) => {
+        request.get('http://localhost:3000/messages', (err, res) => {
+            console.log(res.body)
+            expect(res.statusCode).toEqual(200)
+            done()
+        })
+    })
+
+    it('should return a list that is not empty', (done) => {
+        request.get('http://localhost:3000/messages', (err, res) => {
+            expect(JSON.parse(res.body).length).toBeGreaterThan(1)
+            done()
+        })
+    })
+})
+
+describe('Get messages from a specific user', () => {
+    it('should return 200 OK', (done) => {
+        request.get('http://localhost:3000/messages/Thor', (err, res) => {
+            expect(JSON.parse(res.body)[0].name).toEqual('Thor')
+            done()
+        })
+    })
+})
